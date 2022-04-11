a6c1b2ce6b8e963fe14ee8fc552d7538ec122a5f
Binod Sharma <binodsharma02331@gmail.com>
2022-04-11 23:12:21 +0545

    added bootstrap

--- src/components/GetVehicleContainer.js
+++ src/components/GetVehicleContainer.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { fetchVehicle } from "../redux";
+import { registerVehicle, fetchVehicle } from "../redux";
+import { fetchPayment, addPayment, updatePayment } from "../redux";
 import { makeStyles, createTheme } from "@material-ui/core/styles";
 import { Formik, Form, Field } from "formik";
 import { Button, Paper, Grid, Typography } from "@material-ui/core";
 import { TextField } from "formik-material-ui";
+import { useSelector, useDispatch } from "react-redux";
+
+// import moment from "moment";
+
 // import { Link } from "react-router-dom";
 
-import * as Yup from "yup";
 import { Row, Col } from "react-bootstrap";
+import Header from "./Header";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,59 +34,256 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function RegisterContainer(props) {
+function StatusContainer(props) {
   const classes = useStyles();
-  const initialValues = {
-    name: "",
-    phone: "",
+  const [status, setCurrentStatus] = useState("");
+  const [vnumber, setVnumber] = useState("");
+  const [pay, setPay] = useState("");
+
+  // const [params, setParams] = useState("");
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchVehicle());
+    const interval = setInterval(() => {
+      dispatch(fetchVehicle());
+    }, 9000);
+
+    return () => clearInterval(interval);
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchPayment());
+    const interval = setInterval(() => {
+      dispatch(fetchPayment());
+    }, 9000);
+
+    return () => clearInterval(interval);
+  }, [dispatch]);
+
+  // setParams(queryParams.q);
+  // const location = searchParams.get("location")
+
+  const allPayments = useSelector((state) => state.payment.allPayments);
+
+  const allVehicles = useSelector((state) => state.register.allVehicles);
+  // var vehicleNumber = allVehicles.map((vehicle) => vehicle.createdAt);
+  var vehicleNumber2 = allVehicles.map((vehicle) => vehicle.vehiclenumber);
+
+  console.log("vehicleNumber2", vnumber);
+
+  //console.log(); // 11/3/2019
+  //   let isoDate = vehicleNumber.map((vehicle) =>
+  //     moment(vehicle).format("HHmmss  ")
+  //   );
+
+  var date1 = new Date(vehicleNumber2[0]);
+  var date2 = new Date(vehicleNumber2[4]);
+  var ElapsedSeconds = (date2 - date1) / 1000;
+  var ElapsedHours = ElapsedSeconds / 3600;
+
+  console.log(
+    "vehicle detailsdddddddddddddddddddddddddddddddddd",
+    allPayments
+
+    // moment.format(isoDate[1], "HH:mm:ss  ") -
+    //   moment.format(isoDate[0], "HH:mm:ss  ")
+  );
+
+  const initialCheck = {
     email: "",
     vehicleNumber: "",
   };
-  const validationSchema = Yup.object({
-    name: Yup.string().required("Enter the name"),
-    email: Yup.string().email().required("Enter the email"),
-    phone: Yup.string().required("Enter the phone number"),
-    vehicleNumber: Yup.string().required("Enter the vehicle number"),
-  });
-  const onSubmit = (values, onSubmitProps) => {
-    // console.log("Form data dddddddddddddddddddddddddddddd", values);
-    onSubmitProps.resetForm();
-    props.registerVehicle(
-      values.name,
-      values.email,
-      values.phone,
-      values.vehicleNumber
+
+  var random = Math.floor(Math.random() * 1000000);
+  if (pay === "valid") {
+    var paymentStatus = allPayments.map((payment) => {
+      console.log("paymenssssssssssssssssddt", payment.epaystatus);
+      if (
+        (payment.epaystatus === false) &
+        (payment.vehiclenumber === vnumber)
+      ) {
+        var entryAction = (
+          <div>
+            <form action="https://uat.esewa.com.np/epay/main" method="POST">
+              <input value={payment.efee} name="tAmt" type="hidden" />
+              <input value={payment.efee} name="amt" type="hidden" />
+              <input value="0" name="txAmt" type="hidden" />
+              <input value="0" name="psc" type="hidden" />
+              <input value="0" name="pdc" type="hidden" />
+              <input value="EPAYTEST" name="scd" type="hidden" />
+              <input value={random} name="pid" type="hidden" />
+              <input
+                value="http://localhost:3000/payment/esewa_payment_success?q=su"
+                type="hidden"
+                name="su"
+              />
+              <input
+                value="http://localhost:3000/payment/esewa_payment_failed?q=fu"
+                type="hidden"
+                name="fu"
+              />
+              <Button
+                type="submit"
+                value="Submit"
+                variant="contained"
+                color="primary"
+              >
+                pay entry fee={payment.efee}
+              </Button>
+            </form>
+          </div>
+        );
+        return <>{entryAction}</>;
+      } else if (
+        (payment.epaystatus === true) &
+        (payment.vehiclenumber === vnumber)
+      ) {
+        entryAction = (
+          <div>
+            <h1>entry fee paid</h1>
+          </div>
+        );
+        return <>{entryAction}</>;
+      }
+
+      return <></>;
+    });
+    var fineStatus = allPayments.map((payment) => {
+      console.log("paymenssssssssssssssssddt", payment.epaystatus);
+      if (
+        (payment.finestatus === false) &
+        (payment.vehiclenumber === vnumber)
+      ) {
+        var fineAction = (
+          <div>
+            <form action="https://uat.esewa.com.np/epay/main" method="POST">
+              <input value={payment.fine} name="tAmt" type="hidden" />
+              <input value={payment.fine} name="amt" type="hidden" />
+              <input value="0" name="txAmt" type="hidden" />
+              <input value="0" name="psc" type="hidden" />
+              <input value="0" name="pdc" type="hidden" />
+              <input value="EPAYTEST" name="scd" type="hidden" />
+              <input value={random} name="pid" type="hidden" />
+              <input
+                value="http://localhost:3000/payment/esewa_payment_success?q=su"
+                type="hidden"
+                name="su"
+              />
+              <input
+                value="http://localhost:3000/payment/esewa_payment_failed?q=fu"
+                type="hidden"
+                name="fu"
+              />
+              <Button
+                type="submit"
+                value="Submit"
+                variant="contained"
+                color="primary"
+              >
+                pay fine ={payment.fine}
+              </Button>
+            </form>
+          </div>
+        );
+        return <>{fineAction}</>;
+      } else if (
+        (payment.finestatus === true) &
+        (payment.vehiclenumber === vnumber)
+      ) {
+        fineAction = (
+          <div>
+            <h4>No fine</h4>
+          </div>
+        );
+        return <>{fineAction}</>;
+      }
+
+      return <></>;
+    });
+  }
+
+  console.log("registerd status   ", status);
+  if (status === "success") {
+    var registerAction = (
+      <div>
+        {/* <h4>Pay entry fee {vnumber}</h4>
+        <h4>{paymentStatus}</h4>
+        <h4>Pay fine fee {fineStatus}</h4>
+        <br /> */}
+        <main class="page payment-page">
+          <section class="clean-block payment-form dark">
+            <div class="container">
+              <div class="block-heading">
+                <h2 class="text-info">Payment</h2>
+              </div>
+              <form>
+                <div class="products">
+                  <h3 class="title">Checkout</h3>
+                  <div class="item">
+                    <span class="price">{paymentStatus}</span>
+                    <p class="item-name"> Entry fee</p>
+                  </div>
+                  <div class="item">
+                    <span class="price">$120</span>
+                    <p class="item-name"> Fine</p>
+                  </div>
+                  <div class="total">
+                    <span>Total</span>
+                    <span class="price">$320</span>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </section>
+        </main>
+      </div>
     );
-    // props.addPost(values.name);
+  }
+  if (status === "failure") {
+    registerAction = (
+      <div>
+        <h1>Vehicle not registered</h1>
+      </div>
+    );
+  }
+
+  const onCheck = (values, onCheckProps) => {
+    console.log("Form data dddddddddddddddddddddddddddddd", values);
+    onCheckProps.resetForm();
+
+    let check = vehicleNumber2.includes(values.vehicleNumber);
+    if (check) {
+      check = "success";
+      setCurrentStatus(check);
+      setVnumber(values.vehicleNumber);
+      setPay("valid");
+    } else {
+      check = "failure";
+      setCurrentStatus(check);
+    }
   };
   return (
     <>
-      <div className={classes.root}>
-        <Grid container direction="row" justify="center" alignItems="center">
-          <Paper className={classes.paper} elevation={2}>
-            <Typography>Register Vehicle</Typography>
-            <Typography>{props.msg}</Typography>
-
-            <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              onSubmit={onSubmit}
-            >
-              {(formik) => {
-                return (
-                  <>
-                    <Form>
-                      <Row>
-                        <Col>
-                          <Field
-                            component={TextField}
-                            label="name"
-                            name="name"
-                            size="medium"
-                            id="standard-size-small"
-                            InputProps={{ notched: "true" }}
-                          />
-                        </Col>
+      <div>
+        <main class="page login-page">
+          <section class="clean-block clean-form dark">
+            <div class="container">
+              <div class="block-heading">
+                <br />
+                <h2 class="text-info">Log In</h2>
+                <p>Lorem ipsum dolor sit amet, c in, mattis vitae leo.</p>
+              </div>
+              <Formik
+                initialValues={initialCheck}
+                //   validationSchema={validationSchema}
+                onSubmit={onCheck}
+              >
+                {(formik) => {
+                  return (
+                    <>
+                      <Form class="Form-group">
+                        {/* <Row> */}
                         <Col>
                           <Field
                             component={TextField}
@@ -92,17 +294,7 @@ function RegisterContainer(props) {
                             InputProps={{ notched: "true" }}
                           />
                         </Col>
-                        <Col>
-                          <Field
-                            component={TextField}
-                            label="Phone"
-                            type="phone"
-                            name="phone"
-                            size="small"
-                            id="standard-size-small"
-                            InputProps={{ notched: "true" }}
-                          />
-                        </Col>
+
                         <Col>
                           <Field
                             component={TextField}
@@ -113,53 +305,38 @@ function RegisterContainer(props) {
                             InputProps={{ notched: "true" }}
                           />
                         </Col>
-                        <Typography align="left" variant="h6" noWrap>
-                          {/* <h6>
-                            Already have an account?
-                            <Link to="/login">
-                              <p>Log in</p>
-                            </Link>
-                          </h6> */}
-                        </Typography>
+                        <Typography
+                          align="left"
+                          variant="h6"
+                          noWrap
+                        ></Typography>
                         <Col>
+                          <br />
                           <Button
                             type="submit"
                             disabled={!formik.isValid}
                             variant="contained"
                             color="primary"
                           >
-                            Submit
+                            Check
                           </Button>
                         </Col>
-                      </Row>
-                    </Form>
-                  </>
-                );
-              }}
-            </Formik>
-          </Paper>
-        </Grid>
+                        {/* </Row> */}
+                      </Form>
+                    </>
+                  );
+                }}
+              </Formik>
+            </div>
+          </section>
+        </main>
+        <div>
+          <h1>{registerAction}</h1>
+          {/* <h1>{vehicleNumber}</h1> */}
+        </div>
       </div>
     </>
   );
 }
 
-const mapStatetoProps = (state) => {
-  return {
-    name: state.register.name,
-    email: state.register.email,
-    phone: state.register.phone,
-    vehicleNumber: state.register.vehicleNumber,
-    msg: state.register.msg,
-  };
-};
-
-const mapDispatchtoProps = (dispatch) => {
-  return {
-    registerVehicle: function (name, email, phone, vehicleNumber) {
-      dispatch(registerVehicle(name, email, phone, vehicleNumber));
-    },
-  };
-};
-
-export default connect(mapStatetoProps, mapDispatchtoProps)(RegisterContainer);
+export default StatusContainer;
